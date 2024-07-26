7dc93e8f848dfc0f40c8c4b7693c0311468c3e68
parkhyomi <phyomi3@naver.com>
2024-07-26 16:01:01 +0900

    [class 보조 생성자 사용]

--- app/src/main/java/com/example/a726android/MainActivity.kt
+++ app/src/main/java/com/example/a726android/MainActivity.kt
@@ -25,11 +25,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-class SmartDevice{
-    val name = "android Tv"
-    val category = "Enterainment"
-    var deviceStatus = "online"
+class SmartDevice(val name:String,val category:String){
+    var deviceStatus="online"
 
+    constructor(name: String,category: String,statusCode:Int):this(name, category) {
+        deviceStatus= when(statusCode){
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
     fun turnOn(){
         println("Smart device is turned on.")
     }
